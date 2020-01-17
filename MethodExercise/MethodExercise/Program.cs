5976ac1e453804019417d5e329f47ffc189c38e0
Jack Fagan <jhfagan579@gmail.com>
2020-01-17 10:37:58 -0600

    Created prompts, inputs, and readout

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's Your Name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What's Your Favorite Color?");
+            var favColor = Console.ReadLine();
+            Console.WriteLine("What's Your Favorite Animal?");
+            var favAnimal = Console.ReadLine();
+            Console.WriteLine("What's Your Favorite Band's Name?");
+            var favBand = Console.ReadLine();
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Favorite Color: {favColor}");
+            Console.WriteLine($"Favorite Animal: {favAnimal}");
+            Console.WriteLine($"Favorite Band: {favBand}");
+        }
+    }
+}
